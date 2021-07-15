3bb8a9caa619d3a6752fdcaa5204b7633b02c43c
sanghyeok ko <kosang5719@gmail.com>
2021-07-15 11:56:13 +0900

    2nd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like ko ko ko SangHyeok! ko ko ko');
+    res.send('I like ko ko ko ko SangHyeok! ko ko ko ko');
 });
 
 app.get('/start', function(req, res){
