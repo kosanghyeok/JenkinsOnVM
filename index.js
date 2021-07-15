1d658b191e26bd77bd5ac339372a0ddd8c57e377
sanghyeok ko <kosang5719@gmail.com>
2021-07-15 16:24:42 +0900

    1st

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like IU IU IU');
+    res.send('I like SangHyeok');
 });
 
 app.get('/start', function(req, res){
