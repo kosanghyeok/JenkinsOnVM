ca89905b0cff4b7d179ea0235857ba9cdd5cc713
sanghyeok ko <kosang5719@gmail.com>
2021-07-15 13:25:06 +0900

    5th

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like girls gneration');
+    res.send('I like once twice');
 });
 
 app.get('/start', function(req, res){
